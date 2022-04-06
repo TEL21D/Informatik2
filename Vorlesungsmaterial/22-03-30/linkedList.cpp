b2ede82583835333b42779a8ae0f8e4153a1edb2
toj11001 <tobias.joschko@gmx.de>
2022-04-06 17:27:55 +0200

    Hilfsfunktionen und variablen linkedList

--- Vorlesungsmaterial/22-03-30/linkedList.cpp
+++ Vorlesungsmaterial/22-03-30/linkedList.cpp
@@ -51,14 +51,30 @@ struct linkedList
     linkedListElement *_head; // zeigt auf den Anfang der Liste
     linkedListElement *_tail; // zeigt auf das Ende der Liste
 
+    unsigned int _size;
+
+
     //  Konstruktor wird einmal beim intialisieren der Variable aufgerufen
     linkedList() {
         _head = new linkedListElement();
+        _size = 0;
+    }
+    int size() {
+        // Gibt die Anzahl der Elemente in der Liste zurueck
+        return _size;
+    }
+    bool empty() {
+        // Giht zuruck ob die Liste leer ist
+        return _size == 0;
     }
     void push_back(int data) {
+        // Fuegt ein Element ans Ende der Liste
         _head->push_back(data);
+        _size++;
     }
     int get() {
+        // Gibt das letzte Element der Liste zurueck
+        // -1 wenn die Liste leer ist
         if (_head->empty())
         {
             return -1;
@@ -71,6 +87,8 @@ struct linkedList
         return temp->_data;
     }
     int pop_back() {
+        // Gibt das letzte Element der Liste zurueck und loescht es
+        // -1 wenn die Liste leer ist
         if (_head->empty())
         {
             return -1;
@@ -91,18 +109,20 @@ struct linkedList
             prev->_next = temp->_next;
         }
         delete temp;
+        _size--;
 
         return temp_data;
 
     }
     void print() {
+        // Gibt den Inhalt der Liste auf der Konsole aus
         linkedListElement *temp = _head;
 
         while(!temp->empty()) {
             std::cout << temp->_data << " -> ";
             temp = temp->_next;
         }
-        std::cout << "nullptr";
+        std::cout << "DummyElement\n";
     }
 };
 
@@ -116,19 +136,36 @@ int main(int argc, char const *argv[])
     // el3._data = 3;
     // el3._next = nullptr;
     linkedList llist;       // Konstruktor Aufruf
+    std::cout << "llist.empty(): " << llist.empty() << "\n";
+    std::cout << "llist.size(): " << llist.size() << "\n";
     // llist._head = &el1;
     // llist._tail = &el3;
     llist.push_back(1);
     llist.push_back(2);
     llist.push_back(3);
 
+
     std::cout << "Hello Linked List \n";
     // std::cout << llist._head->_data << " -> " << llist._head->_next->_data  <<
     //     " -> " << llist._head->_next->_next->_data << " -> " << std::endl;
     llist.print();
+    std::cout << "llist.size(): " << llist.size() << "\n";
     llist.push_back(5);
     llist.print();
+    std::cout << "llist.empty(): " << llist.empty() << "\n";
+    std::cout << "llist.size(): " << llist.size() << "\n";
+
     std::cout << "\nllist.popback(): " << llist.pop_back() << "\n";
+    std::cout << "\nllist.get(): " << llist.get() << "\n";
+
+    std::cout << "llist.size(): " << llist.size() << "\n";
+
+    // liste mit popback verkleinern
+    std::cout << "\nllist.popback(): " << llist.pop_back() << "\n";
+    std::cout << "\nllist.popback(): " << llist.pop_back() << "\n";
+    std::cout << "\nllist.popback(): " << llist.pop_back() << "\n";
+    std::cout << "llist.size(): " << llist.size() << "\n";
+
     std::cout << "\nllist.get(): " << llist.get() << "\n";
     llist.print();
     return 0;
