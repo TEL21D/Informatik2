3f69151ebad27a6384c8ac2d287ee0f784bf5cde
toj11001 <tobias.joschko@gmx.de>
2022-04-28 22:51:15 +0200

    loesung avl

---
+++ Uebungsaufgaben/Bäume/avl-baeume/loesung/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "tree.hpp"
+using namespace std;
+
+void test_insertions();
+
+int main() {
+  test_insertions();
+
+  return 0;
+}
+
+void test_insertions() {
+  Tree t1;
+
+  t1.insert(42);    /*         42                23           */
+  t1.insert(23);    /*        /  \              /  \          */
+  t1.insert(77);    /*       23  77            5    42        */
+  t1.insert(5);     /*      /  \       ==>    /    /  \       */
+  t1.insert(38);    /*     5    38           1    38   77     */
+  t1.insert(1);     /*    /                                   */
+                    /*   1                                    */
+  cout << t1.dotString() << endl;
+}
---
+++ Uebungsaufgaben/Bäume/avl-baeume/loesung/tree.cpp
@@ -0,0 +1,41 @@
+#include "tree.hpp"
+
+#include "element.hpp"
+using namespace std;
+
+Tree::Tree() : root{new Element()} {}
+
+void Tree::insert(int key) {
+  root->insert(key);
+  root = rotate(root);
+}
+
+string Tree::inOrderValues() {
+  return root->inOrderValues();
+}
+
+string Tree::pathToElement(int key) {
+  string out;
+  Element * current = root;
+
+  while (current->_key != key) {
+    if (current->empty()) { return "X"; }
+    if (current->_key > key) {
+      current = current->_left;
+      out += "l";
+    }
+    else {
+      current = current->_right;
+      out += "r";
+    }
+  }
+
+  return out;
+}
+
+string Tree::dotString() {
+  string out = "digraph G {\n";
+  out += root->dotString();
+  out += "}\n";
+  return out;
+}
