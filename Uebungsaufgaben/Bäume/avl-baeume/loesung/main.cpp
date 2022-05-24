ac9d4bd0fb01061c2771cc93489f176ff32b2aa6
toj11001 <tobias.joschko@gmx.de>
2022-05-24 21:15:17 +0200

    debug data

--- Uebungsaufgaben/Bäume/avl-baeume/loesung/main.cpp
+++ Uebungsaufgaben/Bäume/avl-baeume/loesung/main.cpp
@@ -3,13 +3,37 @@
 using namespace std;
 
 void test_insertions();
+void test_insertions2();
 
 int main() {
   test_insertions();
+  test_insertions2();
 
   return 0;
 }
 
+void test_insertions2() {
+  Tree t1;
+  t1.insert(10);
+  t1.insert(9);
+  t1.insert(11);
+  t1.insert(8);
+  t1.insert(12);
+  t1.insert(7);
+  cout << t1.dotString() << endl;
+  t1.insert(13);
+  cout << t1.dotString() << endl;
+  t1.insert(6);
+  cout << t1.dotString() << endl;
+  // t1.insert(14);
+  // t1.insert(5);
+  // t1.insert(15);
+  // t1.insert(4);
+  // t1.insert(17);
+
+  cout << t1.inOrderValues() << endl;
+  cout << t1.dotString() << endl;
+}
 void test_insertions() {
   Tree t1;
 
