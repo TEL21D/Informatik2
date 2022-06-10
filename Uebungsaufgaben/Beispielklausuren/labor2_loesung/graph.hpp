9273a06b59fcecd1fa2cddff4b8dce60fac75191
toj11001 <tobias.joschko@gmx.de>
2022-06-10 12:12:00 +0200

    typo aus graph.hpp behoben

--- Uebungsaufgaben/Beispielklausuren/labor2_loesung/graph.hpp
+++ Uebungsaufgaben/Beispielklausuren/labor2_loesung/graph.hpp
@@ -8,7 +8,7 @@
 struct User {
   std::string _username;  // Name des Benutzers
   std::string _town;  // Wohnort des Benutzers
-  std::string _unicourse;  // Wohnort des Benutzers
+  std::string _unicourse;  // Studiengang des Benutzers
   std::vector<User *> _friends;
 
   User(std::string name, std::string town, std::string course) : _username{name}, _town{town}, _unicourse{course} {}
